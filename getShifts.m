49426c214ed03d934f976baca470dbaf263d639a
pia.stammer <pia.stammer@kit.edu>
2020-11-05 15:41:42 +0100

    Extended getShifts for several beams

--- getShifts.m
+++ getShifts.m
@@ -12,21 +12,24 @@ end
 
 elseif strcmp(correlation_type,'ray')
 %% Raywise
-bixelsPerRay_accum= [0 cumsum([stf.numOfBixelsPerRay])];
 shift_3D = zeros(D,3);
 shift_rep = zeros(D,length(mup),3);
-for i=1:stf.numOfRays
+
+for r=1:numel(stf)
+    bixelsPerRay_accum= [0 cumsum([stf(r).numOfBixelsPerRay])];
+for i=1:stf(r).numOfRays
     shift_3D=quasimvnrnd(mue,diag(sigmae),D,'halton');
-    for j=1:stf.numOfBixelsPerRay
+    for j=1:stf(r).numOfBixelsPerRay
      shift_rep(:,bixelsPerRay_accum(j)+i,:)=shift_3D;
     end
+end
 end
 
 elseif strcmp(correlation_type,'beam')
 %% Beamwise
 shift_3D = zeros(D,3);
 shift_rep = zeros(D,length(mup),3);
-for r=1:size(rot_angles,1)
+for r=1:numel(stf)
     shift_3D=quasimvnrnd(mue,diag(sigmae),D,'halton');
     for i=1:K(r+1)
      shift_rep(:,K_sum(r)+i,:)=shift_3D;
